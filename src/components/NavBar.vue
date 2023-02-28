e9d8416514ece8fa920a8d7a355288de703e778e
Rose Odhiambo <odhiamborose466@gmail.com>
2023-02-28 21:50:06 +0300

    feat:add navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,52 +1,64 @@
 <template>
-  <main class="bg-neutral-50 border-b sticky top-0 z-[99999]">
-    <div class="mx-auto max-w-7xl flex justify-between py-4">
-      <!-- Logo -->
-      <router-link :to="{ name: 'home' }" class="flex items-center gap-4">
-        <!-- draw a v logo  svg-->
-        <Logo />
-        <h1>
-          <span class="text-2xl font-bold text-colo">Validient</span>
-          <span class="text-2xl font-bold">Law</span>
-        </h1>
-      </router-link>
-      <!-- Navlinks -->
-      <nav class="flex items-center">
-        <ul class="flex gap-6 items-center">
-          <li v-for="link in headerLinks" :key="link.name">
-            <router-link :to="link.href" class="text-lg font-lg text-neutral-500 hover:text-neutral-900">
-              {{ link.name }}
-            </router-link>
-          </li>
-        </ul>
-      </nav>
-      <!-- CTAs -->
-      <div v-if="authStateAuthenticated" class="flex gap-4 items-center">
-        <div v-for="({ routeName, title }) of dashBoardLinks">
-          <router-link v-if="routeName === 'client'" :to="{ name: routeName }"
-            class="text-md font-medium hover:text-neutral-900 px-4 py-2 rounded-full border flex items-center gap-2">
-            @
-            {{ authStateUser?.username }}
-            <img :src="authStateUser?.avatar!" alt="" class="h-8 w-8 rounded-full">
+  <main class="bg-neutral-50 border-b sticky top-0 lg:z-[99999] ">
+      <div class="mx-auto max-w-7xl flex justify-between py-2 lg:py-4">
+          <!-- Logo -->
+          <router-link :to="{ name: 'home' }" class="flex items-center gap-2 lg:gap-4">
+              <!-- draw a v logo  svg-->
+              <Logo />
+              <h1>
+                  <span class="text-md lg:text-2xl font-bold text-colo">Validient</span>
+                  <span class="text-md lg:text-2xl font-bold">Law</span>
+              </h1>
           </router-link>
-          <router-link v-else :to="{name:routeName}" class="text-lg font-lg text-neutral-500 hover:text-neutral-900">
-              {{ title }}
-            </router-link>
-        </div>
+          <!-- Navlinks -->
+          <nav class=" flex items-center">
+              <ul :class="showMenu ? 'flex' : 'hidden'" class=" flex-col
+                  mt-8
+                  space-y-4 lg:flex lg:gap-6 lg:items-center">
+                  <li v-for="link in headerLinks" :key="link.name">
+                      <router-link :to="link.href"
+                          class="text-md font-mediumlg:text-lg lg:font-lg text-neutral-500 hover:text-neutral-900">
+                          {{ link.name }}
+                      </router-link>
+                  </li>
+              </ul>
+              <div @click="toggleMenu"
+                  class="lg:hidden justify-center flex items-center mr-2 ml-1 cursor-pointer transition">
+                  <button type="button">
+                      <fa icon="fa fa-bars" class="text-2xl"></fa>
+                  </button>
+              </div>
+
+          </nav>
+          <!-- CTAs -->
+          <div v-if="authStateAuthenticated" class="flex gap-2 items-center  md:gap-4">
+              <div v-for="({ routeName, title }) of dashBoardLinks">
+                  <router-link v-if="routeName === 'client'" :to="{ name: routeName }"
+                      class="text-md font-medium hover:text-neutral-900 px-2 py-1 lg:px-4 lg:py-2 rounded-full border flex items-center lg:gap-2">
+                      @
+                      {{ authStateUser?.username }}
+                      <img :src="authStateUser?.avatar!" alt="" class="h-8 w-8 rounded-full">
+                  </router-link>
+                  <router-link v-else :to="{ name: routeName }"
+                      class="text-lg font-lg text-neutral-500 hover:text-neutral-900">
+                      {{ title }}
+                  </router-link>
+              </div>
 
 
+          </div>
+          <div v-if="!authStateAuthenticated" class="flex gap-2 lg:gap-4">
+              <router-link :to="{ name: 'login' }"
+                  class="text-md font-medium hover:text-neutral-900 px-4 py-2 rounded-full border border-color1 text-color1">
+                  Login
+              </router-link>
+              <router-link :to="{ name: 'register' }"
+                  class="text-md font-medium hover:text-neutral-900 bg-color1 text-color3 py-2 px-4 rounded-full">
+                  Register
+              </router-link>
+          </div>
+
       </div>
-      <div v-if="!authStateAuthenticated" class="flex gap-4">
-        <router-link :to="{ name: 'login' }"
-          class="text-md font-medium hover:text-neutral-900 px-4 py-2 rounded-full border border-color1 text-color1">
-          Login
-        </router-link>
-        <router-link :to="{ name: 'register' }"
-          class="text-md font-medium hover:text-neutral-900 bg-color1 text-color3 py-2 px-4 rounded-full">
-          Register
-        </router-link>
-      </div>
-    </div>
   </main>
 </template>
 <script setup lang="ts">
@@ -58,20 +70,20 @@ const store = useAuthStore();
 const { authStateAuthenticated, authStateUser } = storeToRefs(useAuthStore())
 const headerLinks = ref([
   {
-    name: "Home",
-    href: "/",
+      name: "Home",
+      href: "/",
   },
   {
-    name: "About",
-    href: "/about",
+      name: "About",
+      href: "/about",
   },
   {
-    name: "Contact",
-    href: "/contact",
+      name: "Contact",
+      href: "/contact",
   },
   {
-    name: "FAQs",
-    href: "/faq",
+      name: "FAQs",
+      href: "/faq",
   },
 ],);
 type RouteV = {
@@ -82,35 +94,38 @@ type RouteV = {
 const dashBoardLinks = computed((): RouteV[] => {
   const pages: RouteV[] = []
   store.authStateUser?.roles?.forEach((role) => {
-    if (role.name === 'user') {
-      pages.push({
-        name: 'client',
-        title: 'Profile',
-        routeName: 'client'
-      })
-    }
-    if (role.name === 'lawyer') {
-      pages.push({
-        name: 'lawyer',
-        title: 'Lawyer dashboard',
-        routeName: 'lawyer'
-      })
-    }
-    if (role.name === 'admin') {
-      pages.push({
-        name: 'admin',
-        title: 'admin dashboard',
-        routeName: 'admin'
-      })
-    }
+      if (role.name === 'user') {
+          pages.push({
+              name: 'client',
+              title: 'Profile',
+              routeName: 'client'
+          })
+      }
+      if (role.name === 'lawyer') {
+          pages.push({
+              name: 'lawyer',
+              title: 'Lawyer dashboard',
+              routeName: 'lawyer'
+          })
+      }
+      if (role.name === 'admin') {
+          pages.push({
+              name: 'admin',
+              title: 'admin dashboard',
+              routeName: 'admin'
+          })
+      }
 
   })
-pages.push({
-    name: 'logout',
-    title: 'Logout',
-    routeName: 'logout'
+  pages.push({
+      name: 'logout',
+      title: 'Logout',
+      routeName: 'logout'
   })
   return pages.sort((a, b) => a.title.localeCompare(b.title))
 })
+// responsive navbar
+const showMenu = ref(false)
+const toggleMenu = () => (showMenu.value = !showMenu.value)
 
 </script>
--- src/components/admin/AdminProfile.vue
+++ src/components/admin/AdminProfile.vue
@@ -6,50 +6,59 @@ const firstName = ref('Felix')
 const lastName = ref('Orinda')
 </script>
 <template>
-    <div class="">
-        <h1 class="text-center text-3xl font-rubik">My Profile</h1>
-
-        <form class="max-w-xl mx-auto shadow rounded bg-slate-300 p-2">
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-                    First Name
-                </label>
-                <input v-model="firstName"
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="name" type="text" placeholder="Enter your name">
+    <div class="flex gap-3 flex-col p-3">
+        
+        <form class="w-full mx-auto shadow rounded bg-slate-100 p-2 flex gap-5">
+            <div class="flex-1 mt-12 flex flex-col gap-3 items-center">
+                <img class="rounded-full w-20 h-20" src="https://xsgames.co/randomusers/assets/avatars/male/10.jpg" alt="">
+                <button class="bg-sky-600 rounded p-3 text-white">Connect lawyers</button>
+                <fa class="rounded bg-slate-400 capitalize font-semibold text-center text-2xl"
+                    icon="fa fa-edit"><span>Edit</span></fa>
             </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-                    Last Name
-                </label>
-                <input v-model="lastName"
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="name" type="text" placeholder="Enter your name">
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-                    Email
-                </label>
-                <input v-model="email"
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="email" type="email" placeholder="Enter your email address">
-            </div>
-            <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="message">
-                    Comments
-                </label>
-                <textarea
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="message" rows="5" placeholder="Enter your message"></textarea>
-            </div>
-            <div class="flex items-center justify-between">
-                <button
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    type="button">
-                    Save
-                </button>
+            <div class="flex-1 flex flex-col">
+                <h1 class="text-center text-3xl font-rubik">My Profile</h1>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                        First Name
+                    </label>
+                    <input v-model="firstName"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="name" type="text" placeholder="Enter your name">
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                        Last Name
+                    </label>
+                    <input v-model="lastName"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="name" type="text" placeholder="Enter your name">
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                        Email
+                    </label>
+                    <input v-model="email"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email" type="email" placeholder="Enter your email address">
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="message">
+                        Comments
+                    </label>
+                    <textarea
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="message" rows="5" placeholder="Enter your message"></textarea>
+                </div>
+                <div class="flex items-center justify-between">
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button">
+                        Save
+                    </button>
+                </div>
             </div>
         </form>
+
     </div>
 </template>
 <style scoped></style>
